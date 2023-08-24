a27d94eedd761e05e19e574426c9d3979f0a467c
Anan Abed <a28_a3@yahoo.com>
2023-08-24 17:42:35 +0300

    first commit

--- program.sh
+++ program.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+source globals/definitions.sh
+source scripts/search/search.sh
+
+DBFileName=$1
+
+if [[ $# -lt 1 ]]
+then
+    echo "Invalid number of arguments while starting the program, terminating"
+    exit
+fi
+    
+#searchDBFile
+#searchLogFile
+findRecord
+
+
