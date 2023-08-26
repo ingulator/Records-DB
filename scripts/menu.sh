bfaee83d5f79aa897bd9b60c0aea8bb84a4c18c5
Oleg Klimov <o1egkl@gmail.com>
2023-08-26 21:41:59 +0300

    minor fix

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -15,37 +15,42 @@ while true
             echo "Add records"
             result=$(addRecord)
             logToFile $result
-            exit
             ;;
          
             Delete)
             echo "Delete records" 
             result = $(deleteVinyl)
             logToFile $result
-
             ;;   
         
             Search)
             echo "Search"
             result = $(findRecord)
             logToFile $result
-
             ;;
         
             UpdateName)
             echo "Update record names"
+            result = $(UpdateName)
+            logToFile $result
             ;;
 
             UpdateAmount)
             echo "Update record amounts"
+            result = $(UpdateAmount)
+            logToFile $result
             ;;   
         
             PrintAmount)
             echo "Print record amounts"
+            result = $(PrintAmount)
+            logToFile $result
             ;;
         
             PrintAll)
             echo "Print all records"
+            result = $(findRecord)
+            logToFile $result
             ;;  
 
             Quit)
